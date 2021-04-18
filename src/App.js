84cb86d9a32ca76cf2a70cf9e4cbcd6ee5d146e0
Rivka-Halili <rivkahalili18@gmail.com>
2021-04-18 23:15:50 +0300

    organization crud

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Admin, EditGuesser,ListGuesser, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 
 import { UserList,UserEdit,UserCreate,UserShow  } from './users';
+import {OrganizationCreate }from './organizations/Create'
+import {OrganizationEdit }from './organizations/Edit'
+import {OrganizationList }from './organizations/List'
 import UserIcon from '@material-ui/icons/People';
 //import authProvider from './authProvider';
 import { createMuiTheme } from '@material-ui/core/styles';
@@ -14,12 +17,14 @@ const theme = createMuiTheme({
 const App = () => (
     <Admin theme ={theme}  dataProvider={simpleRestProvider('http://localhost:4000/api') }>
 
-    <Resource name="organizations" 
+<Resource name="organizations" 
               options={{ label: 'ארגונים' }}
-              list={ListGuesser}
-              edit ={EditGuesser}
+              list={OrganizationList}
+              edit ={OrganizationEdit}
+              create={OrganizationCreate}
               show={UserShow}
               icon={UserIcon}/>
+   
 
     <Resource name="programs" 
               options={{ label: 'תוכניות' }}
